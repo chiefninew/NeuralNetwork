3257fa963ab5a1d9e885bfe5bfb93b1f08bd8358
Nino Paul Cervantes <oninpaul15@gmail.com>
2021-07-17 20:36:01 +0800

    simple neural network

--- script.js
+++ script.js
@@ -1,25 +1,85 @@
-const network = new brain.NeuralNetwork({
-  hiddenLayers: [3,4]
+BrowserFS.install(window);
+BrowserFS.configure({
+  fs: "LocalStorage"
+}, function(e) {
+  if (e) {
+    throw e;
+  }
+});
+var fs = require('fs');
+let defaultData = {
+  data: [
+    {
+      input: { r: 0, g: 0, b: 0 },
+      output: [1]
+    },
+    {
+      input: { r: 1, g: 1, b: 1 },
+      output: [0]
+    }
+  ]
+};
+let contents = [];
+fs.readFile('/learned.json', function(err, learnedData) {
+  if(!learnedData) {
+    fs.writeFile('/learned.json', JSON.stringify(defaultData), function(err) {
+      if(err) {
+        console.log('ERROR WRITING', err);
+      }
+    });
+  }
 });
 
-network.train([
-  {
-    input: [0, 0],
-    output: [0]
-  },
-  {
-    input: [0, 1],
-    output: [1]
-  },
-  {
-    input: [1, 0],
-    output: [1]
-  },
-  {
-    input: [1, 1],
-    output: [0]
-  }
-])
 
-const diagram = document.getElementById('diagram');
-diagram.innerHTML = brain.utilities.toSVG(network);
+fs.readFile('/learned.json', function(err, learnedData) {
+  if(learnedData) {
+    const learned = JSON.parse(learnedData.toString());
+    if(learned && learned.data) {
+      contents = learned.data;
+      console.log('CONTENTS', contents);
+    }
+  }
+});
+
+if(!contents) {
+  console.log('No Data Found.');
+}
+
+const network = new brain.NeuralNetwork();
+network.train(contents);
+
+const colorElem = document.getElementById('color');
+const guessElem = document.getElementById('guess');
+let color;
+setRandomColor();
+
+function setRandomColor() {
+  color = {
+    r: Math.random(),
+    g: Math.random(),
+    b: Math.random(),
+  };
+  const guess = network.run(color)[0];
+  guessElem.style.color = guess > 0.5 ? '#FFF' : '#000';
+  colorElem.style.backgroundColor = `rgba(${color.r * 255}, ${color.g * 255}, ${color.b * 255})`;
+}
+
+function chooseColor(value) {
+  contents.push({
+    input: color,
+    output: [value],
+  });
+  setRandomColor();
+}
+
+function onSave() {
+  console.log('DATA', contents);
+  defaultData = {
+    data: contents,
+  };
+  fs.writeFile('/learned.json', JSON.stringify(defaultData), function(err) {
+    if(err) {
+      console.log('ERROR WRITING', err);
+    }
+  });
+}
